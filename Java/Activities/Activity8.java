2edfbe26086272cf3894dd574894f5cb5c835b6f
sanjupujeri <sanjubpujeri26@gmail.com>
2025-12-03 17:33:32 +0530

    Add files via upload
    Activity 8 completed

---
+++ Java/Activities/Activity8.java
@@ -0,0 +1,37 @@
+package activity1;
+
+class CustomException extends Exception {
+	private String message;
+
+	public CustomException(String message) {
+
+		this.message = message;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+}
+
+public class Activity8 {
+
+	public static void exceptionTest(String Value) throws CustomException {
+
+		if (Value == null) {
+			throw new CustomException("Input value cannot be null please enter some value!");
+		} else {
+			System.out.println("Received: " + Value);
+		}
+	}
+
+	public static void main(String[] args) {
+        try {
+            exceptionTest("Will print to console");
+            exceptionTest(null);   // This will throw CustomException
+        } catch (CustomException e) {
+            System.out.println("Custom Error: " + e.getMessage());
+        }
+    }
+}
